a65c9d77432944d3d0ba3807144b662b97ec0599
lis mendoza <lis.mendoza.cc@gmail.com>
2020-10-16 01:05:22 -0300

    commit 16/10/2020

--- validacionlogin.php
+++ validacionlogin.php
@@ -32,35 +32,4 @@ if(count($resultado)==1){
 
 
 
-
-
-
-session_start(); 
-	
-	/*creamos un elemento en el array $_SESSION y le asignamos un valor.*/
-	function session_set($nombre, $valor){
-		$_SESSION[$nombre]=$valor;
-	} 
-
-	function session_get($nombre){
-		if(isset($_SESSION[$nombre])){  //isset verifica la existencia de la variable
-			return $_SESSION[$nombre];
-		} else {
-			return FALSE;
-		}
-	}
-
-	function session_var_unset($nombre){
-		if(isset($_SESSION[$nombre])){  //isset verifica la existencia de la variable
-			unset($_SESSION[$nombre]);
-			return TRUE;
-		} else {
-			return FALSE;
-		}
-	}
-	
-	function session_terminate(){
-		session_unset();//Destruye todas las variables asociadas con la sesion en curso.
-		session_destroy();//Destruye toda la info relacionada con la sesion incluyendo cookies.
-	} 
 ?>
