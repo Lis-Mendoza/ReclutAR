151122cd045bb65776f0b842cc2394d1df854424
Lis-Mendoza <lis.mendoza.cc@gail.com>
2020-10-26 13:54:32 -0300

    Base de Datos Remota

--- config.php
+++ config.php
@@ -1,16 +1,9 @@
 <?php
-<<<<<<< HEAD
-	//database CONSTANTES
-=======
 	/*//database CONSTANTES
->>>>>>> 88381c9 ( MySQL Remoto)
 	define('SQL_SERVER','localhost');
 	define('SQL_USER','root');
 	define('SQL_PASS','');
 	//define('SQL_DB',);
-<<<<<<< HEAD
-	define('SQL_DB','reclutar');
-=======
 	define('SQL_DB','reclutar');*/
 
 define('SQL_SERVER','remotemysql.com');
@@ -18,5 +11,4 @@ define('SQL_SERVER','remotemysql.com');
 	define('SQL_PASS','L2TXN7w31p');
 	//define('SQL_DB',);
 	define('SQL_DB','Jn7QCmab2D');
->>>>>>> 88381c9 ( MySQL Remoto)
 ?>
