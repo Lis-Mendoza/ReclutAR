bd39567c63be10a255fdb2cdae75a3d68d72a083
Lis-Mendoza <lis.mendoza.cc@gail.com>
2020-10-26 13:36:20 -0300

    primer comit cambio de compu

--- config.php
+++ config.php
@@ -1,8 +1,22 @@
 <?php
+<<<<<<< HEAD
 	//database CONSTANTES
+=======
+	/*//database CONSTANTES
+>>>>>>> 88381c9 ( MySQL Remoto)
 	define('SQL_SERVER','localhost');
 	define('SQL_USER','root');
 	define('SQL_PASS','');
 	//define('SQL_DB',);
+<<<<<<< HEAD
 	define('SQL_DB','reclutar');
+=======
+	define('SQL_DB','reclutar');*/
+
+define('SQL_SERVER','remotemysql.com');
+	define('SQL_USER','Jn7QCmab2D');
+	define('SQL_PASS','L2TXN7w31p');
+	//define('SQL_DB',);
+	define('SQL_DB','Jn7QCmab2D');
+>>>>>>> 88381c9 ( MySQL Remoto)
 ?>
--- session.php
+++ session.php
@@ -3,6 +3,7 @@
 	session_start(); 
 	
 	/*creamos un elemento en el array $_SESSION y le asignamos un valor.*/
+<<<<<<< HEAD
 	function session_set($e_mail, $valor){
 		$_SESSION[$e_mail]=$valor;
 	}
@@ -11,14 +12,30 @@
 	function session_get($e_mail){
 		if(isset($_SESSION[$e_mail])){  //isset verifica la existencia de la variable
 			return $_SESSION[$e_mail];
+=======
+	function session_set($name, $valor){
+		$_SESSION[$name]=$valor;
+	}
+	
+	/*Buscamos un elemento en el array $_SESSION y devolvemos su valor*/
+	function session_get($name){
+		if(isset($_SESSION[$name])){  //isset verifica la existencia de la variable
+			return $_SESSION[$name];
+>>>>>>> 88381c9 ( MySQL Remoto)
 		} else {
 			return FALSE;
 		}
 	}
 	/*Buscamos un elemento en el array $_SESSION y eliminamos su valor*/
+<<<<<<< HEAD
 	function session_var_unset($e_mail){
 		if(isset($_SESSION[$e_mail])){  //isset verifica la existencia de la variable
 			unset($_SESSION[$e_mail]);
+=======
+	function session_var_unset($name){
+		if(isset($_SESSION[$name])){  //isset verifica la existencia de la variable
+			unset($_SESSION[$name]);
+>>>>>>> 88381c9 ( MySQL Remoto)
 			return TRUE;
 		} else {
 			return FALSE;
